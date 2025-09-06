d779cc28287d97c1398eb8ac3c4e9dce3f0b713f
mainadante <mainadante@gmail.com>
2025-09-06 20:26:10 +0100

    Exercises week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+      
+        // Console.Write("What is the magic number? ");
+        // string answer1 = Console.ReadLine();
+        // int magicNumber = int.Parse(answer1);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        string answer2 = Console.ReadLine();
+        int guess = int.Parse(answer2);
+
+        while (guess != magicNumber)
+        {
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+
+            Console.Write("What is your guess? ");
+            answer2 = Console.ReadLine();
+            guess = int.Parse(answer2);
+        }
+        
+        Console.WriteLine("You guessed it!");
+        
+
+
+       
+        
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,10 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> listNumbers = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+
+            if (number != 0)
+            {
+                listNumbers.Add(number);
+            }
+
+        }
+        
+        Console.WriteLine("You entered:");
+        foreach (int n in listNumbers)
+        {
+            Console.WriteLine(n);
+        }
+
+
     }
 }
 
